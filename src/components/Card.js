321a8775b25b7b760532299e018c7b7c574d580e
Coleen Huang <coleenhuang@gmail.com>
2020-12-10 12:14:36 -0800

    set up basic homepage for the app

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Card.module.css';
+
+const Card = () => {
+    return (
+        <div className={styles.card}>
+            <p>This is a Card</p>
+        </div>
+    )
+}
+
+export default Card;
