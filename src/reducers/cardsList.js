9f5cc7cba2109acdca41630477171b0af647b341
Coleen Huang <coleenhuang@gmail.com>
2020-12-10 17:12:46 -0800

    modified cardsList reducer to exclude unneccessary information

--- src/reducers/cardsList.js
+++ src/reducers/cardsList.js
@@ -3,7 +3,7 @@ import { FETCH_CARDS } from '../actions/types'
 export default function(state=[], action) {
     switch(action.type) {
         case FETCH_CARDS:
-            return [...state, action.payload];
+            return [...state, action.payload.data];
         default:
             return state
     }
