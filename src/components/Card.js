e39a5373482804a27ea094368490d433deb583ad
Coleen Huang <coleenhuang@gmail.com>
2020-12-11 18:25:01 -0800

    added flipping action to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Card.module.css';
 
 const Card = () => {
+    const [show, setShow] = useState(true)
+    //FIXME: need to somehow nest the front and back of the card on top of one another
     return (
-        <div className={'flipper'}>
-            <div className={`${styles.front} ${styles.card}`}>Front</div>
-            <div className={`${styles.back} ${styles.card}`}>Back</div>
+        <div className={styles.flipper} onClick={() => setShow(!show)}>
+            <div className={`${styles.front} ${styles.card_face} ${show?styles.visible:styles.hidden}`}>Front</div>
+            <div className={`${styles.back} ${styles.card_face} ${show?styles.hidden:styles.visible}`}>Back</div>
         </div>
     )
 }
