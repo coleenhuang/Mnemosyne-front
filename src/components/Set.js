413a7ccd27bd354434d41130338b0d8ded4daa31
Coleen Huang <coleenhuang@gmail.com>
2020-12-30 17:59:11 -0800

    connected the setContainer to redux state

---
+++ src/components/Set.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Set = (props) => {
+    return (
+    <div>  
+        Set
+
+    </div>
+    )
+}
+
+export default Set;
+
--- src/components/SetContainer.js
+++ src/components/SetContainer.js
@@ -1,9 +1,30 @@
+import { render } from '@testing-library/react';
 import React from 'react';
+import { connect } from 'react-redux';
+import Set from './Set'
 import { useParams } from 'react-router-dom';
 
-const SetContainer = () => {
-    let params = useParams();
-    return <div>Current parameter is {params.name}</div>
+const SetContainer = (props) => {
+    return <div>
+        {renderSets(props.setList)}
+    </div>
 }
 
-export default SetContainer;
+function renderSets(setList) {
+    if (setList.length > 0) {
+        return setList.map((set, index) => 
+        <Set key={index} name={set.set_name} set_id={set.id}/>)
+    }
+    else {
+        return null
+    }
+    
+}
+
+function mapStateToProps(state) {
+    return {
+        setList: state.sets
+    }
+}
+
+export default connect(mapStateToProps)(SetContainer);
